2762ef37bb671504457cbe078ab8dba7079ba8c8
Alexandr <karyagin68@gmail.com>
2023-12-10 19:44:43 +0300

    added task2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//функцию, которая обнулит чётные элементы массива
+void ZeroElements(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            array[i] = 0;
+        }
+    }
+}
+void PrintArray(int[] array)
+{
+    foreach (int e in array)
+    {
+        Console.Write($"{e} ");
+    }
+}
+int[] array = { 2, 5, 6, 8, 9, 3, 1, 6 };
+ZeroElements(array);
+PrintArray(array);
